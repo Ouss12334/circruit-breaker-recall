4bdac0671faa44356a2494d2772f3a8d715916c6
unknown <oussama@q.fr>
2025-04-10 11:46:58 +0200

    init

---
+++ reading-client/src/main/java/com/slope/reading_client/ReadingClientApplication.java
@@ -0,0 +1,13 @@
+package com.slope.reading_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingClientApplication.class, args);
+	}
+
+}
---
+++ reading-client/src/test/java/com/slope/reading_client/ReadingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slope.reading_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
